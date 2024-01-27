a2a6e74f3b1bbca4726c007d011a9df7965fa58f
Mark Mendoza <markmendoza@MacBook-Pro-2.local>
2024-01-27 00:09:15 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import ProductList from "./components/productList/ProductList";
+import Contact from "./components/contact/Contact";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+  );
 };
 
 export default App;
